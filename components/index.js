9972f751f9db8fe74fc0aff3a0b89dcb420993c1
Musab Shaikh <musabs2802@gmail.com>
2022-06-12 08:18:56 +0530

    integrate sanity client library

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar'
+export { default as HeroBanner } from './HeroBanner'
+export { default as Footer } from './Footer'
+export { default as FooterBanner } from './FooterBanner'
